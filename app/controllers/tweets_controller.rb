73e5024354e3678f9dcc6d0b78608c139908567e
shota0412 <sh.v.w0412@gmail.com>
2021-02-15 23:49:01 +0900

    ツイート保存機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,6 +6,18 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+     @tweet = Tweet.new(tweet_params)
+     @tweet.user_id = current_user.id
+     @tweet.save
+     redirect_to tweet_path(@tweet)
+  end
+
   def show
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text, :image)
   end
 end
